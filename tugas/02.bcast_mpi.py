f110214577326feea43cc600f24eed6d44f45d27
Azril Nurfaizi <azrilstudent13@gmail.com>
2020-04-17 11:37:37 +0700

    Revert "Update 02.bcast_mpi.py"
    This reverts commit 72a8680b044fc24357293a60b174d045e8592586.

--- tugas/02.bcast_mpi.py
+++ tugas/02.bcast_mpi.py
@@ -1,22 +1,19 @@
 # import mpi4py
-from mpi4py import MPI
+
 
 # buat COMM
-comm = MPI.COMM_WORLD
+
 
 # dapatkan rank proses
-rank = comm.Get_rank()
+
 
 # dapatkan total proses berjalan
-size = comm.Get_size()
+
 
 # jika saya rank 0 maka saya akan melakukan broadscast
-if rank == 0:
-    msg = 'echo'
+if rank == :
+
 	
 # jika saya bukan rank 0 maka saya menerima pesan
 else:
-	msg = None
-
-data = comm.bcast(msg,root=0)
-print('Broadcast {}, data {}'.format(rank, msg))
+	
